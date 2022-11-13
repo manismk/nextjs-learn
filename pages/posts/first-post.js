b2af8affaf5773e9e6647cdd9d07df17a34bcb47
manismk <manismkmech@gmail.com>
2022-11-13 20:09:13 +0530

    chore; added new page

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const FirstPost = () => {
+  return (
+    <div>
+      <h1>First Post</h1>
+      <Link href="/">Back</Link>
+    </div>
+  );
+};
+
+export default FirstPost;
